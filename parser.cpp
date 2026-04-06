d7370cdbe83dd462c06b3fed1dcdd8335a2f473d
Thishaan <udia@wisc.edu>
2026-04-05 22:23:43 -0500

    implemented basic logic

--- parser.cpp
+++ parser.cpp
@@ -1,20 +1,23 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 #include <unordered_map>
+#include <sstream>
 
 using str_view = std::string_view; 
 using json_vector = std::vector<str_view>;
 using json_map = std::unordered_map<str_view, json_vector>;
 
+using ss = std::stringstream;
 
 
-
-enum state{
+typedef enum state{
     START,
     KEY,
     VALUE,
-};
+} state_t;
 struct json_parser{
 
 
@@ -27,16 +30,74 @@ struct json_parser{
             exit(EXIT_FAILURE); //exit program rn
         }
          
+        std::string line;
+        state_t current_state = START;
 
-        ///TODO: implement parsing logic here
+        std::string curr_key =  NULL;
+        size_t pos_start;
+        size_t pos_end;
 
+        while(std::getline(in, line)){
+             ss line_stream(line);
+           
+        switch(current_state){
+
+                if(line.find('{') != std::string_view::npos){
+                        current_state = KEY; 
+                }
+                    goto key;  // bcause ima primitive piece of shit
+                    break;
+        key:     case KEY:
+              
+                 if ((pos_start = line.find("\"")) != std::string::npos) {
+
+                        if((pos_end = line.find("\"", pos_start)) != std::string::npos){
+                            curr_key =  line.substr(pos_start, pos_end);
+                             
+                        }else{
+                            exit(EXIT_FAILURE);
+                        }
+                        goto  value;  // why the fuck not
+                }else{
+                    exit(EXIT_FAILURE);
+                }
+                
+                break;
+        value : case VALUE:
+                    //parse value
+                    if((pos_start = line.find(":")) != std::string::npos){
+                        
+                         if((pos_start = line.find("[")) != std::string::npos){
+
+
+                        }
+
+                        // case of a string
+                        else if((pos_start = line.find("\"")) != std::string::npos){
+
+                        }
+
+                        // assume misc case like a number or something. 
+                        else{
+
+
+                        }
+
+                    }else{
+                            exit(EXIT_FAILURE);
+                    }
+
+                    break;
+            }
+        }
 
     }
 
     private:
         std::ifstream in;
         json_map data;
-} 
+        state_t state;
+};
 
 
 int main(){
